4a2f7382a2135e1fdc31489b35a828da77354983
Chris Everest <ceverest@covermymeds.com>
2015-10-30 10:22:21 -0400

    Add pypi preparations

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hq-code-deployer',
-    version='1.0.1',
+    version='1.0.2',
     url='',
     include_package_data=True,
     license='',
